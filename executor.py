04d36b3dd79de14af1fabe6e09fda129385d51d0
jbonet <jbl4@alumnes.udl.cat>
2018-04-06 11:44:14 +0200

    Script runners

---
+++ executor.py
@@ -0,0 +1,15 @@
+# coding = utf8
+
+import os
+
+testFolder = "testbed"
+ncores = ['2', '4']
+
+def main():
+    testFiles = [f for f in os.listdir(testFolder) if os.path.isfile(os.path.join(testFolder, f))]
+    for cores in ncores:
+        for testFile in testFiles:            
+            os.system("./run.sh {0} {1}".format(testFile, cores))
+
+if __name__ == '__main__':
+    main()
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+qsub -S /bin/bash -V -pe smp 4 -wd $HOME/results/OptimalFence/OMP -N OptimalFenceOMP_$1_$2 -v OMP_NUM_THREADS=$2 -b y $HOME/hpc-project-2018/OptimalFence $HOME/hpc-project-2018/testbed/$1
